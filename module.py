5bd2e8c27ffcba3e38dfa35a8509a7cd840944bc
Harrison <treeblue3@gmail.com>
2025-02-26 15:36:43 +0000

    added a function to plot the trace of objects in the all class

--- module.py
+++ module.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-GRAVITY = 0.
+GRAVITY: float = -10.
 
 class object:
     def __init__(self, pos:list=[0.,0.], vel:list=[0.,0.], mass:float=1) -> None:
@@ -30,7 +31,7 @@ class interact:
     def __init__(self, o1:object, o2:object) -> None:
         self.o1 = o1
         self.o2 = o2
-        self.range = 1.
+        self.range = 10.
         if self.dist() < self.range:
             self.collide()
     
@@ -50,37 +51,64 @@ class interact:
 class all:
     def __init__(self) -> None:
         self.objects: list = []
+        self.history: dict = {}
 
     def track(self, obj: object) -> None:
         self.objects.append(obj)
+        self.history[obj] = [obj.get_pos()]
     
-    def locate(self):
-        for obj in self.objects:
-            print(obj.get_pos())
+    def locate(self) -> None:
+        print("\nLocations:")
+        for obj in self.history:
+            print(self.history[obj][len(self.history[obj])-5:])
+    
+    def traces(self) -> None:
+        xs = []
+        ys = []
+        for obj in self.history:
+            x = []
+            y = []
+            for xy in self.history[obj]:
+                x.append(xy[0])
+                y.append(xy[1])
+            xs.append(x)
+            ys.append(y)
+        for i in range(len(xs)):
+            plt.scatter(xs[i],ys[i])
+        plt.show()
 
     def update(self) -> None:
         for ob1 in self.objects:
             for ob2 in self.objects:
                 if ob1 != ob2:
-                    print(f'interaction between {ob1} and {ob2}')
+                    # print(f'interaction between {ob1} and {ob2}')
                     interact(ob1, ob2)
         for ob in self.objects:
             ob.update()
+            self.history[ob].append(ob.get_pos())
 
 
 
 
 def main() -> None:
     blob =          object(pos=[0.,0.])
-    other_blob =    object(pos=[5.,0.])
+    blob.push([1.9,0.])
+    other_blob =    object(pos=[100.,0.])
 
     scene = all()
     scene.track(blob)
     scene.track(other_blob)
+
+    
+
+    MAXTIME = 100
+    for t in range(MAXTIME):
+        scene.update()
     scene.locate()
 
-
+    scene.traces()
 
 if __name__ == "__main__":
     main()
+
 
